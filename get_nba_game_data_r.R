23092b25831d80610efc47b03b5b4b3656203b9c
hansenjeremy <jeremyxhansen@byui.edu>
2021-01-13 17:00:37 -0700

    this all works

--- get_nba_game_data_r.R
+++ get_nba_game_data_r.R
@@ -81,4 +81,67 @@ df$game_type <- with(df, ifelse(date_game >= playoff_startDate,
 df$box_score_text <- NULL
 
 
+df$winner <- with(df, ifelse(visitor_pts > home_pts, 
+                             visitor_team_name, home_team_name))
+df$loser <- with(df, ifelse(visitor_pts < home_pts, 
+                            visitor_team_name, home_team_name))
+
+
+teams <- sort(unique(df$visitor_team_name))
+standings <- data.frame(team = teams, stringsAsFactors = FALSE)
+standings$conf <- c("East", "East", "East", "East", "East",
+                    "East", "West", "West", "East", "West",
+                    "West", "East", "West", "West", "West",
+                    "East", "East", "West", "West", "East",
+                    "West", "East", "East", "West", "West",
+                    "West", "West", "East", "West", "East")
+standings$div <- c("Southeast", "Atlantic", "Atlantic", "Southeast", "Central",
+                   "Central", "Southwest", "Northwest", "Central", "Pacific",
+                   "Southwest", "Central", "Pacific", "Pacific", "Southwest",
+                   "Southeast", "Central", "Northwest", "Southwest", "Atlantic",
+                   "Northwest", "Southeast", "Atlantic", "Pacific", "Northwest",
+                   "Pacific", "Southwest", "Atlantic", "Northwest", "Southeast")
+
+
+
+standings$win <- 0; standings$loss <- 0
+for (i in 1:nrow(standings)) {
+  standings$win[i]  <- sum(df$winner == standings$team[i], na.rm = T)
+  standings$loss[i] <- sum(df$loser  == standings$team[i], na.rm = T)
+}
+
+standings$wl_pct <- with(standings, win / (win + loss))
+
+View(standings)
+
+east_standings <- standings %>% 
+  group_by(conf) %>% 
+  dplyr::summarise(team,
+                   conf,
+                   div,
+                   win,
+                   loss,
+                   wl_pct)
+
+east_standings1 <- arrange(filter(east_standings, conf == "East"), desc(wl_pct))
+east_standings1$`conf == "East"` <-  NULL
+
+View(east_standings1)
+
+west_standings <- standings %>% 
+  group_by(conf) %>% 
+  dplyr::summarise(team,
+                   conf,
+                   div,
+                   win,
+                   loss,
+                   wl_pct)
+
+west_standings1 <- arrange(filter(west_standings, conf == "West"), desc(wl_pct))
+west_standings1$`conf == "Wast"` <-  NULL
+
+View(west_standings1)
+
+
+
 
